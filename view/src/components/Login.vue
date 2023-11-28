4e6a1f9a52b12ff567a1b53e9c44bd37a1f773c5
阿志 <1342798349@qq.com>
2023-11-28 14:15:00 +0800

    登陆框样式调整

--- view/src/components/Login.vue
+++ view/src/components/Login.vue
@@ -1,27 +1,44 @@
 <script setup>
 import useStore from '@/stores'
 import { storeToRefs } from 'pinia'
+import { Key, User } from '@element-plus/icons-vue'
+import { ref } from 'vue'
 const { user } = useStore()
 const { isLogin } = storeToRefs(user)
-const dialogVisible = ref(false)
+
+// 表单数据
+const loginForm = ref({
+  user: '',
+  pwd: ''
+})
+
 </script>
 
 <template>
   <div>
-    <el-dialog v-model="isLogin" title="Tips" width="30%" :before-close="handleClose">
-      <span>This is a message</span>
-      <template #footer>
-        <span class="dialog-footer">
-          <el-button @click="isLogin = false">Cancel</el-button>
-          <el-button type="primary" @click="isLogin = false">
-            Confirm
-          </el-button>
-        </span>
-      </template>
+    <el-dialog title="登陆" center width="300px" v-model="isLogin">
+      <el-form status-icon size="large" :model="loginForm" >
+        <el-form-item>
+          <el-input v-model="loginForm.user" placeholder="请输入账户" 
+          clearable :prefix-icon="User"
+          size="large"/>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="loginForm.pwd" placeholder="请输入密码" 
+          clearable :prefix-icon="Key"
+          size="large"/>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width:100%;">登陆</el-button>
+        </el-form-item>
+      </el-form>
     </el-dialog>
   </div>
 </template>
 
-<style lang=''>
-
+<style lang='scss'>
+.el-form{
+  //display: flex;
+  // flex: 1;
+}
 </style>
